bc97c4f840b3bc8dc1507922a4bea3ee17f855a7
joenwoke <jnwoke@hawk.iit.edu>
2025-06-09 16:47:59 -0700

    Updated Player card with basic layout of card

---
+++ src/components/PlayerCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './PlayerCard.css';
+
+const PlayerCard = ({ name, rating, position, club, nation, image }) => {
+  return (
+    <div className="player-card">
+      <div className="player-header">
+        <span className="rating">{rating}</span>
+        <span className="position">{position}</span>
+      </div>
+      <img src={image} alt={name} className="player-image" />
+      <h3 className="player-name">{name}</h3>
+      <p className="club">{club}</p>
+      <p className="nation">{nation}</p>
+    </div>
+  );
+};
+
+export default PlayerCard;
