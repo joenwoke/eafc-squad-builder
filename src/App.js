78534e543709bb67af5ca70ae2ed970266f93636
joenwoke <jnwoke@hawk.iit.edu>
2025-06-09 11:18:32 -0700

    Updated App.js with Imports from React router, layout and home path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div>
-      <h1>EAFC Squad Builder</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
